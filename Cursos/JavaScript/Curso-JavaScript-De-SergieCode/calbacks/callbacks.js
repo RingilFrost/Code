7361f8302856a886561603ccbcef3ad4e1b5edf5
Alejandro Duran <36010084+Du5kH0r1z0n@users.noreply.github.com>
2023-09-22 19:25:52 -0300

    Add files via upload

---
+++ Cursos/JavaScript/Curso-JavaScript-De-SergieCode/calbacks/callbacks.js
@@ -0,0 +1,10 @@
+function mostrarConsola(num){
+    console.log(num)
+}
+
+function calcular(num1, num2, callback){
+    let suma = num1 + num2
+    callback(suma)
+}
+
+calcular(1,3,mostrarConsola)
